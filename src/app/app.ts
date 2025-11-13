80e82f9fb60ea5285858637194e33dedb729ac6f
Samar-Elfeky <samarelfeky69@gmail.com>
2025-11-13 12:49:32 +0200

    create

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePage } from "./home-page/home-page";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [ HomePage],
+  templateUrl: './app.html',  
+  styleUrls: ['./app.scss']  
 })
 export class App {
-  protected readonly title = signal('biban-najd');
+
 }
