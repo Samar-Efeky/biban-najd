aad99d7e4b8f5203b5084070461fbbeef49072d8
Samar-Elfeky <samarelfeky69@gmail.com>
2025-11-14 19:40:23 +0200

    update

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroSection } from '../hero-section/hero-section';
+import { OurServices } from "../our-services/our-services";
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeroSection],
+  imports: [HeroSection, OurServices],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
